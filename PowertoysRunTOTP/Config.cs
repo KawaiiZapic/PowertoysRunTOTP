2ad40b8991d473722b48ff130fc5cabd525c22f1
KawaiiZapic <kawaiizapic@zapic.moe>
2026-04-01 20:11:01 +0800

    fix: accidentialy rename the config file name

--- PowertoysRunTOTP/Config.cs
+++ PowertoysRunTOTP/Config.cs
@@ -28,13 +28,17 @@ namespace Community.PowerToys.Run.Plugin.TOTP {
         private static readonly string DataDirectoryV1 = Environment.ExpandEnvironmentVariables("%LOCALAPPDATA%") + "\\Microsoft\\PowerToys\\PowerToys Run\\Settings\\Plugins\\TOTP\\";
         private static readonly string ConfigPathV1 = DataDirectoryV1 + "Config.json";
 
+        // PluginJsonStorage<T> use reflection to determine the name of config file
+        // rename namespace or type name will cause file path changed
+        class OTPList { }
+
         public static void Migrate() {
             if (!new FileInfo(ConfigPathV1).Exists)
                 return;
 
             var ConfigV2 = Core.ConfigMigratorV1.MigrateFromFile(ConfigPathV1);
 
-            var ConfigV2Storage = new PluginJsonStorage<Core.StructV1>();
+            var ConfigV2Storage = new PluginJsonStorage<OTPList>();
             new StoragePowerToysVersionInfo(ConfigV2Storage.FilePath, 1).Close();
 
             File.WriteAllBytes(ConfigV2Storage.FilePath, ConfigV2);
@@ -49,13 +53,17 @@ namespace Community.PowerToys.Run.Plugin.TOTP {
         private static readonly string ConfigPathV2 = DataDirectoryV2 + "OTPList.json";
         private static readonly string ConfigVersionPathV2 = DataDirectoryV2 + "OTPList_version.txt";
 
+        // PluginJsonStorage<T> use reflection to determine the name of config file
+        // rename namespace or type name will cause file path changed
+        class AuthenticatorsList { }
+
         public static void Migrate() {
             if (!new FileInfo(ConfigPathV2).Exists)
                 return;
 
             var ConfigV3 = Core.ConfigMigratorV2.MigrateFromFile(ConfigPathV2);
 
-            var ConfigV3Storage = new PluginJsonStorage<Core.StructV3>();
+            var ConfigV3Storage = new PluginJsonStorage<AuthenticatorsList>();
             new StoragePowerToysVersionInfo(ConfigV3Storage.FilePath, 1).Close();
 
             File.WriteAllBytes(ConfigV3Storage.FilePath, ConfigV3);
