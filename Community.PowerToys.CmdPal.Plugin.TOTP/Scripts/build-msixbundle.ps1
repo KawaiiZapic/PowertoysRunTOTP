dd72fa121ad55de419a8c52184567b79bbcacd7f
KawaiiZapic <kawaiizapic@zapic.moe>
2026-04-04 11:40:06 +0800

    ci: ready for store publishing

---
+++ Community.PowerToys.CmdPal.Plugin.TOTP/Scripts/build-msixbundle.ps1
@@ -0,0 +1,61 @@
+$ErrorActionPreference = 'Stop'
+
+$projectRoot = (Split-Path $MyInvocation.MyCommand.Path | Get-Item).Parent.FullName
+
+Push-Location $projectRoot
+
+$BuildOptions = "-p:GenerateAppxPackageOnBuild=true"
+if ($args -contains "-sideload") {
+    $BuildOptions += " -p:PublishUnsignedPackage=true"
+    Write-Host "Building for sideloading (unsigned package)"
+} else {
+    Write-Host "Building for store submission (signed package)"
+}
+if ($args -contains "-debug") {
+    $BuildOptions += " --configuration Debug"
+    Write-Host "Debug profile selected"
+} else {
+    $BuildOptions += " --configuration Release"
+    Write-Host "Release profile selected"
+}
+
+function Execute-Command {
+    param(
+        [string]$Command
+    )
+    
+    Write-Host "$> $Command"
+    Invoke-Expression -Command $Command
+    $exitCode = $LASTEXITCODE
+    
+    if ($exitCode -ne 0) {
+        throw "Command failed with exit code $exitCode"
+    }
+    
+    return $output
+}
+
+Remove-Item -Path "AppPackages\" -Recurse -Force -ErrorAction SilentlyContinue
+
+Execute-Command "dotnet build -p:Platform=x64 $BuildOptions"
+Execute-Command "dotnet build -p:Platform=arm64 $BuildOptions"
+
+$x64MsixPath = Get-ChildItem -Path "AppPackages\*_x64_*" -Filter "*.msix" -Recurse | Select-Object -First 1
+$arm64MsixPath = Get-ChildItem -Path "AppPackages\*_arm64_*" -Filter "*.msix" -Recurse | Select-Object -First 1
+ 
+if ($x64MsixPath -and $arm64MsixPath) {
+    $x64FileName = $x64MsixPath.Name
+    $arm64FileName = $arm64MsixPath.Name
+    $bundleMappingContent = @'
+[Files]
+"{0}" "{1}"
+"{2}" "{3}"
+'@ -f $x64MsixPath, $x64FileName, $arm64MsixPath, $arm64FileName
+
+    $bundleMappingContent | Out-File -FilePath ".\AppPackages\bundle_mapping.txt" -Encoding UTF8
+    $OutputFileName = ".\AppPackages\{0}" -f $x64FileName.Replace("_x64", "").Replace('.msix', '_Bundle.msixbundle')
+    Execute-Command "makeappx bundle /v /f .\AppPackages\bundle_mapping.txt /p $OutputFileName"
+} else {
+    Write-Host "msix packages not found"
+    Exit 1
+}
