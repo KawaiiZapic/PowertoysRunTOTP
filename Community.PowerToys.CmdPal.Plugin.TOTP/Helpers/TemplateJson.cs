fa2b8b20ccabded264ca8aa20cf40de2aa88d28b
KawaiiZapic <kawaiizapic@zapic.moe>
2026-04-04 14:33:00 +0800

    fix: use code generation for json string sanitize

--- Community.PowerToys.CmdPal.Plugin.TOTP/Helpers/TemplateJson.cs
+++ Community.PowerToys.CmdPal.Plugin.TOTP/Helpers/TemplateJson.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Community.PowerToys.CmdPal.Plugin.TOTP.Helpers {
+
+    [JsonSerializable(typeof(string))]
+    public partial class TemplateJsonStringContext: JsonSerializerContext {
+    }
     internal class TemplateJson {
 
         public static string Replace(string content, Dictionary<string, string>? kv) {
             var result = content;
             if (kv != null) {
                 foreach (var pair in kv) {
-#pragma warning disable IL2026, IL3050
-                    result = result.Replace($"\"${{{pair.Key}}}\"", JsonSerializer.Serialize(pair.Value));
-#pragma warning restore IL2026, IL3050
+                    result = result.Replace($"\"${{{pair.Key}}}\"", JsonSerializer.Serialize(pair.Value, TemplateJsonStringContext.Default.String));
                 }
             }
             return result;
